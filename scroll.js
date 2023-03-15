a4e6f8a2b2e0cce482dddd6ee46b6f18ab3b1eb0
Gregory0401 <grigoriosdart@gmail.com>
2023-03-15 09:00:40 +0200

    final

---
+++ scroll.js
@@ -0,0 +1,18 @@
+// ------------------Скрипт для скролла ссылок--------------
+{
+  /* <script>
+      jQuery("a").on("click", function (e) {
+        e.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+      });
+    </script> */
+}
+
+// -----Скрипт для скролла ссылок находящихся в подгружаемом контенте-----
+
+// <script>
+//   jQuery(document).on("click", "a", function (e) {
+//     e.preventDefault();
+//     $("html, body").animate({ scrollTop: 0 }, "slow");
+//   });
+// </script>
